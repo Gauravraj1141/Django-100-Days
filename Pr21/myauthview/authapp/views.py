03a279145b54dc19c31d46731e302b7ed86cf14e
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-01-13 22:09:08 +0530

    login requried

--- Pr21/myauthview/authapp/views.py
+++ Pr21/myauthview/authapp/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+
 # here we need to create a view function that show form on the template
+# here we give staff memeber required so only staff member can show this
 
-from django.contrib.auth.views import LoginView
 
-
+@staff_member_required
 def home(request):
     return render(request, "registration/home.html")
 
 
 # here we need to give profile url so we create function here when user login
+# here we give login required so until user doesn't login until profile will not show
+@login_required
 def profile(request):
-    return HttpResponse("hey welcome to your profile")
+    return render(request, "registration/profile.html")
 
 
 # change our password
